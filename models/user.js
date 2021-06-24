9105b238cd1846608448926f15ba000b9ae44526
Muhammad Abuzaid <ibnsaadoosh@gmail.com>
2021-06-25 01:26:56 +0200

    Passport

--- models/user.js
+++ models/user.js
@@ -1,27 +1,19 @@
 var express = require('express');
-
 var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+
 var router = express.Router();
 
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-  username:
-  {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password:
-  {
-    type: String,
-    required: true
-  },
   admin:
   {
     type: Boolean,
     default: false
   }
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
