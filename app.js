37f50e8243168161b3ae1b339956c82a7c9dc71b
Mohamed Saad <ibnsaadoosh@yahoo.com>
2021-07-30 07:14:07 +0200

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -29,6 +30,18 @@ connect.then((db) =>
 });
 
 var app = express();
+
+//secure traffic only
+app.all('*', (req, res, next) => {
+  if(req.secure)
+  {
+    next();
+  }
+  else
+  {
+    res.redirect(307 , 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -50,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
